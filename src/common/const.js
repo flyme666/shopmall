639e49b6ae99fb5365d7bd1a2b52bd65b89c9d9a
flyme666 <23433653@qq.com>
2020-07-23 14:36:18 +0800

    update api

--- src/common/const.js
+++
@@ -1,2 +0,0 @@
-const URL = ''
-export default URL;
--- src/network/request.js
+++ src/network/request.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import url from '@/common/const'
+import url from '@/common/api'
 
 export function request(config) {
   const instance =axios.create({
